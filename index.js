1082327e6bd3a6301099a55161a37b6f9262ae3e
NickisKnowledge <lifechangingstuff@gmail.com>
2019-04-23 12:47:22 -0500

    Add Event listner to form when page loads & fetch repos based on user input

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// CREATE a GithubApi class to used for searching github and keeps all api logic
+// GOAL: 
+  // - Connect to a github api
+  // - Fetch data and return a promise
+  // - Parse that promise to json (javascript object notation)
+  // - Use the parsed json in order to work with the data
+
+
+window.addEventListener('load', function(){
+  // find target, addEventListener fn, parameters event & cb Fn
+  document.querySelector('form').addEventListener('submit', searchGithub)
+})
+
+
+// e == event
+function searchGithub(e){
+  // alert('CONNECTED CORRECTLY')
+  e.preventDefault();
+  // debugger;
+
+  const BASE_URL = 'https://api.github.com'
+  let query = document.getElementById('query').value
+  // EX: https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
+
+  // using concatenetion to create query
+  // fetch(BASE_URL + '/search/repositories?q=' + query)
+  fetch(BASE_URL + `/search/repositories?q=${query}`)
+  .then(resp => console.log(resp.json()))
+
+
+}
