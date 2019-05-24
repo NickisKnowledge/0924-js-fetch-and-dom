eff929043cf9d78f38a1ebb136d7b44dca1b9427
NickisKnowledge <lifechangingstuff@gmail.com>
2019-05-24 12:08:24 -0500

    Change to use class syntax

--- index.js
+++ index.js
@@ -5,54 +5,78 @@
   // - Use the parsed json in order to work with the data
 
 
+  // create GithubApi class
+  class GithubApi {
+    constructor(){
+      this.base_url = 'https://api.github.com'
+    }
+  }
+
+  class SearchGithub {
+    static findRepos(githubObj, searchTerm) {
+      // EX: https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
+
+      // using concatenetion to create query
+      // fetch(BASE_URL + '/search/repositories?q=' + query)
+      fetch(githubObj.base_url + `/search/repositories?q=${searchTerm}`)
+      .then(function(resp) {return resp.json()})
+      .then(objs => Repository.createRepos(objs.items))
+     }
+
+  }
+
+    class Repository {
+      static all = [];
+      constructor(name, html_url, owner, fork) {
+        this.name = name;
+        this.html_url = html_url;
+        this.owner = owner;
+        this.fork = fork;
+
+        Repository.all.push(this)
+      }
+
+      // create repository objs
+      static createRepos(arr) {
+        arr.forEach(obj => new Repository(obj.name, obj.html_url, obj.owner.login, obj.fork))
+
+        Repository.renderRepoList();
+      }
+
+      static renderRepoList() {
+        // grab tbody tags to insert repo info to HTLM (DOM)
+        let tbody = document.getElementById('repo-list')
+      
+        this.all.forEach((obj, i) => {
+        // debugger
+        let val =`
+          <tr>
+            <td>${i+1}</td>
+            <td>${obj.name }</td>
+            <td><a href=${obj.html_url }>${obj.owner }</a></td>
+            <td>${obj.fork }</td>
+          </tr>
+        `
+        tbody.innerHTML += val;
+      })
+    }
+
+    }
+
+
+
+function searchGithub(e){
+  e.preventDefault();
+  const githubApi = new GithubApi() 
+  let query = document.getElementById('query').value
+
+  SearchGithub.findRepos(githubApi, query)
+
+  // clear query input 
+  document.getElementById('query').value = ''
+}
+
 window.addEventListener('load', function(){
   // find target, addEventListener fn, parameters event & cb Fn
   document.querySelector('form').addEventListener('submit', searchGithub)
 })
-
-function renderRepoList(arr) {
-  // clear query input 
-  document.getElementById('query').value = ''
-
-  // grab tbody tags to insert repo info to HTLM (DOM)
-  let tbody = document.getElementById('repo-list')
-
-  arr.forEach((el, i) => {
-    // debugger
-    let val =`
-      <tr>
-        <td>${i+1}</td>
-        <td>${el.name }</td>
-        <td><a href=${el.html_url }>${el.owne.login }</a></td>
-        <td>${el.form }</td>
-      </tr>
-    `
-    tbody.innerHTML += val;
-  })
-
-
-  
-}
-
-// e == event
-function searchGithub(e){
-  // alert('CONNECTED CORRECTLY')
-  e.preventDefault();
-  // debugger;
-
-  const BASE_URL = 'https://api.github.com'
-  let query = document.getElementById('query').value
-
-  // EX: https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
-
-  // using concatenetion to create query
-  // fetch(BASE_URL + '/search/repositories?q=' + query)
-  fetch(BASE_URL + `/search/repositories?q=${query}`)
-    .then(function(resp) {return resp.json()})
-    .then(objs => {
-      debugger
-      renderRepoList(objs.items)
-    })
-
-
-}
